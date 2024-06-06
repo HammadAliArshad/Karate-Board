a2ad273ef1430d0ac98697afab124ed948db5082
Hammad Ali <Hammadaliha75@outlook.com>
2024-06-05 17:27:26 -0700

    Add functionality to sum score and to show panelty icons

--- src/app/components/score-board/board-header/board-header.component.ts
+++ src/app/components/score-board/board-header/board-header.component.ts
@@ -1,3 +1,4 @@
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CountdownModule } from 'ngx-countdown';
@@ -5,23 +6,55 @@ import { CountdownModule } from 'ngx-countdown';
 @Component({
   selector: 'app-board-header',
   standalone: true,
-  imports: [CountdownModule, RouterLink],
+  imports: [CountdownModule, RouterLink, NgIf, NgClass],
   templateUrl: './board-header.component.html',
   styleUrl: './board-header.component.scss',
 })
 export class BoardHeaderComponent {
-  score: any = 0;
+  scoreAka: any = 0;
+  scoreAo: any = 0;
   matchTime: any = 180;
+  currentFoul: any = 0;
+  akaCategoryOne: any = 0;
+  akaCategoryTwo: any = 0;
+  aoCategoryOne: any = 0;
+  aoCategoryTwo: any = 0;
 
-  setNinty(){
-    this.matchTime = 90;
-  }
-  setTwo(){
-    this.matchTime = 120;
-  }
-  setThree(){
-    this.matchTime = 180;
+  setTime(seconds: number) {
+    this.matchTime = seconds;
   }
 
+  aoScore(aoPoint: number) {
+    this.scoreAo += aoPoint;
+  }
 
+  akaScore(akaPoint: number) {
+    this.scoreAka += akaPoint;
+  }
+
+  categoryOneAka() {
+    this.akaCategoryOne += 1;
+    if (this.akaCategoryOne > 4) {
+      this.akaCategoryOne -= this.akaCategoryOne;
+    }
+  }
+  categoryTwoAka() {
+    this.akaCategoryTwo += 1;
+    if (this.akaCategoryTwo > 4) {
+      this.akaCategoryTwo -= this.akaCategoryTwo;
+    }
+  }
+
+  categoryOneAo() {
+    this.aoCategoryOne += 1;
+    if (this.aoCategoryOne > 4) {
+      this.aoCategoryOne -= this.aoCategoryOne;
+    }
+  }
+  categoryTwoAo() {
+    this.aoCategoryTwo += 1;
+    if (this.aoCategoryTwo > 4) {
+      this.aoCategoryTwo -= this.aoCategoryTwo;
+    }
+  }
 }
